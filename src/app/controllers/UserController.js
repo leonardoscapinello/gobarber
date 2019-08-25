70af015bb60cda8cd5f23c7bbc848198d4ce3fcf
Leonardo Scapinello <leonardoscapinello@MacBook-Pro-de-Leonardo.local>
2019-08-25 17:27:37 -0300

    chore: updating user, usercontroller and password hash

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,17 @@ import User from '../models/Users';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
-    return res.json(user);
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+    if (userExist) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
